cfde9c51b130c3233141a054e46224310d12d454
OhGeezSomethin <mustaphawillisonpersonal@gmail.com>
2025-10-23 19:23:57 -0400

    duct taped falloff generation

--- Assets/Scripts/GenerationTest/HeightMapGenerator.cs
+++ Assets/Scripts/GenerationTest/HeightMapGenerator.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public static class HeightMapGenerator
 {
+    static float[,] falloffMap;
+
     public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, Vector2 sampleCenter)
     {
         float[,] values = Noise.NoiseMapGeneration(width, height, settings.noiseSettings, sampleCenter);
 
         AnimationCurve heightCurve_threadsafe = new AnimationCurve(settings.heightCurve.keys);
+
+        if (settings.useFalloff)
+        {
+            if (falloffMap == null || falloffMap.GetLength(0) != width) falloffMap = FalloffGenerator.GenerateFalloffMap(width);
+        }
 
         float minValue = float.MaxValue;
         float maxValue = float.MinValue;
@@ -16,6 +23,8 @@ public static class HeightMapGenerator
         {
             for(int j = 0; j < height; j++)
             {
+                if (settings.useFalloff) values[i, j] = Mathf.Clamp01(values[i, j] - falloffMap[i, j]);
+
                 values[i,j] *= heightCurve_threadsafe.Evaluate(values[i,j]) * settings.heightMulti;
 
                 if(values[i,j] > maxValue) maxValue = values[i,j];
